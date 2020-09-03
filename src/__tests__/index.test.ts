265b2ba91532fdadee232da165e06d5d0bdb2d02
Eryk Slocinski <eryk.slocinski@gmail.com>
2020-09-03 20:06:25 +0200

    Describe changes in test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -13,16 +13,6 @@ describe('Testing prototype select ', () => {
     expect(isEqualToExpectedValue).toBeTruthy();
   });
 
-  it('it return last element from array when first parametr is -1', () => {
-    const arrWithString: Array<string> = ['erq', 'kosik', 'refix'];
-    const expectedValue: Array<string> = ['refix'];
-    const output: Array<string> = arrWithString.select(-1);
-
-    const isEqualToExpectedValue = lodash.isEqual(expectedValue, output);
-
-    expect(isEqualToExpectedValue).toBeTruthy();
-  });
-
   it('is example 1', () => {
     const expectedValue: Array<number> = [1, 2, 3, 4];
     const output: Array<number> = mockedArr.select(1, 5);
@@ -72,9 +62,19 @@ describe('Testing prototype select ', () => {
 describe('Testing Array.prototype.select parameters', () => {
   const mockedArr: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  it('it must return last 5 element from array when first parametr is -5', () => {
+  it('it must return last 5 element from array with numbers when first parametr is -5', () => {
     const expectedValue: Array<number> = [5, 6, 7, 8, 9];
     const output: Array<number> = mockedArr.select(-5);
+
+    const isEqualToExpectedValue = lodash.isEqual(expectedValue, output);
+
+    expect(isEqualToExpectedValue).toBeTruthy();
+  });
+
+  it('it must return last element from array with strings when first parametr is -1', () => {
+    const arrWithStrings: Array<string> = ['erq', 'kosik', 'refix'];
+    const expectedValue: Array<string> = ['refix'];
+    const output: Array<string> = arrWithStrings.select(-1);
 
     const isEqualToExpectedValue = lodash.isEqual(expectedValue, output);
 
